130e5368db261d78ee0423a9fd86fe236430f75d
Shivam <shivammudotia@IN-FVFF8FJZQ05D.local>
2022-04-16 17:00:41 +0530

    refactoring done

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -13,7 +13,7 @@ export class NewsItem extends Component {
                 <img src={!imageUrl?'https://images.livemint.com/img/2022/04/15/600x338/OnePlus_10R_1650012854721_1650012855104.png':imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                        <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description}...</p>
+                    <p className="card-text">{description}</p>
                     <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(time).toGMTString()}</small></p>
                     <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More..</a>
                 </div> 
